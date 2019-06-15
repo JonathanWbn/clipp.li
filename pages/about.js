7e33aa22e8a22d593e5de869007798f94cd3bffb
Jonathan Wieben <joni.wieben@icloud.com>
2019-06-15 13:29:02 +0200

    New: Setup app with next and react

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import { Component } from "react";
+
+import Header from "../components/header";
+
+class AboutPage extends Component {
+  static getInitialProps() {
+    const isServer = typeof window === "undefined";
+    return { isServer };
+  }
+
+  render() {
+    return (
+      <main>
+        <Header />
+        <section>
+          <p>
+            This is another page of the SSR example, you accessed it{" "}
+            <strong>{this.props.isServer ? "server" : "client"} side</strong>.
+          </p>
+          <p>You can reload to see how the page change.</p>
+          <Link href="/">
+            <a>Go to Home</a>
+          </Link>
+        </section>
+      </main>
+    );
+  }
+}
+
+export default AboutPage;
