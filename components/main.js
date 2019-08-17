384baa68b9b65420bb0c1576c3ddd44acd7b3e67
Jonathan Wieben <joni.wieben@icloud.com>
2019-08-17 20:18:57 +0200

    New: Add random generate button

--- components/main.js
+++ components/main.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import classnames from 'classnames'
 import { Box, Grommet, RangeSelector, Stack } from 'grommet'
+import randomstring from 'randomstring'
 
 import { formatSeconds, getYoutubeId } from '../utils/helpers'
 import { useMobile, useWindowSize, useYoutubeVideoDuration } from '../utils/hooks'
@@ -83,6 +84,17 @@ export default function Main() {
   const reset = () => {
     setFormValues(initialFormValues)
     setErrors({})
+  }
+
+  const generateRandomSlug = () => {
+    setFormValues({
+      ...formValues,
+      slug: randomstring.generate({
+        length: 5,
+        charset: 'alphabetic',
+        capitalization: 'lowercase',
+      }),
+    })
   }
 
   return (
@@ -174,6 +186,20 @@ export default function Main() {
               placeholder="URL ending"
               error={errors.slug || ''}
             />
+            <svg
+              className="random"
+              onClick={generateRandomSlug}
+              x="0px"
+              y="0px"
+              width="25"
+              height="25"
+              viewBox="0 0 32 32"
+            >
+              <title>Generate random slug</title>
+              <g id="surface1">
+                <path d="M 16 4 C 10.886719 4 6.617188 7.160156 4.875 11.625 L 6.71875 12.375 C 8.175781 8.640625 11.710938 6 16 6 C 19.242188 6 22.132813 7.589844 23.9375 10 L 20 10 L 20 12 L 27 12 L 27 5 L 25 5 L 25 8.09375 C 22.808594 5.582031 19.570313 4 16 4 Z M 25.28125 19.625 C 23.824219 23.359375 20.289063 26 16 26 C 12.722656 26 9.84375 24.386719 8.03125 22 L 12 22 L 12 20 L 5 20 L 5 27 L 7 27 L 7 23.90625 C 9.1875 26.386719 12.394531 28 16 28 C 21.113281 28 25.382813 24.839844 27.125 20.375 Z "></path>
+              </g>
+            </svg>
           </div>
           <div className="row">
             <Button type="button" onClick={reset} design="secondary">
@@ -245,6 +271,7 @@ export default function Main() {
           display: flex;
           align-items: center;
           justify-content: space-between;
+          position: relative;
         }
 
         .row > :global(*) {
@@ -352,6 +379,20 @@ export default function Main() {
         .thumbnail-fallback {
           margin-left: 50px;
           width: 445px;
+        }
+
+        .random {
+          cursor: pointer;
+          width: 40px;
+          fill: #a4a4a4;
+          transition: 0.2s all;
+          position: absolute;
+          right: 0px;
+          top: 12px;
+        }
+
+        .random:hover {
+          fill: #09f;
         }
 
         @media (max-width: 700px) {
